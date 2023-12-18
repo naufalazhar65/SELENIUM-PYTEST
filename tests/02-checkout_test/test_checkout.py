91007fa947c65b090761472cae3208efd3553d3c
Naufal Azhar <naufalazhar65@gmail.com>
2023-12-18 17:35:34 +0700

    commit

--- tests/02-checkout_test/test_checkout.py
+++ tests/02-checkout_test/test_checkout.py
@@ -32,95 +32,87 @@ def test_checkout(driver):
     input_email.send_keys("naufalazhar65@gmail.com")
     input_password.send_keys("naufal354")
 
-    print(f"Email input value is: {input_email.get_attribute('value')}")
-    assert input_email.get_attribute('value') == "naufalazhar65@gmail.com", f"Email input value mismatch. Actual: {input_email.get_attribute('value')}"
+    login_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[5]/div[1]/div/div/div/div[2]/div/div/form/input')
+    login_button.click()
+    sleep(2)
 
-    print(f"Password input value is: {input_password.get_attribute('value')}")
-    assert input_password.get_attribute('value') == "naufal354", f"Password input value mismatch. Actual: {input_password.get_attribute('value')}"
+    assert 'My Account' in driver.title
+    verify_login_success = driver.find_element(By.TAG_NAME, 'h2')
+    assert verify_login_success.text == 'My Account'
 
+    # Navigate to Cart
+    cart_button = driver.find_element(By.XPATH, "//header/div[@id='main-header']/div[@id='entry_217820']/div[@id='entry_217825']/a[1]/div[1]")
+    cart_button.click()
+    sleep(2)
 
+    # Proceed to Checkout
+    checkout_button = driver.find_element(By.XPATH, '//body/div[1]/div[3]/div[1]/div[2]/div[1]/div[2]/a[1]')
+    checkout_button.click()
+    sleep(2)
 
-    # login_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[5]/div[1]/div/div/div/div[2]/div/div/form/input')
-    # login_button.click()
-    # sleep(2)
+    assert 'Checkout' in driver.title
 
-    # assert 'My Account' in driver.title
-    # verify_login_success = driver.find_element(By.TAG_NAME, 'h2')
-    # assert verify_login_success.text == 'My Account'
+    # Verify Cart Items
+    verify_cart_card = driver.find_element(By.XPATH, '//body/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]')
+    assert verify_cart_card.is_displayed()
 
-    # # Navigate to Cart
-    # cart_button = driver.find_element(By.XPATH, "//header/div[@id='main-header']/div[@id='entry_217820']/div[@id='entry_217825']/a[1]/div[1]")
-    # cart_button.click()
-    # sleep(2)
+    # Verify Product List
+    verify_product_list_1 = driver.find_element(By.XPATH, '//body[1]/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/a[1]')
+    assert verify_product_list_1.text == 'HTC Touch HD'
+    verify_product_list_2 = driver.find_element(By.XPATH, '//body[1]/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/a[1]')
+    assert verify_product_list_2.text == 'iMac'
 
-    # # Proceed to Checkout
-    # checkout_button = driver.find_element(By.XPATH, '//body/div[1]/div[3]/div[1]/div[2]/div[1]/div[2]/a[1]')
-    # checkout_button.click()
-    # sleep(2)
+    check_btn = driver.find_element(By.XPATH, "//body/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[5]/label[1]")
+    check_btn.click()
 
-    # assert 'Checkout' in driver.title
+    continue_btn = driver.find_element(By.XPATH, "//button[@id='button-save']")
+    continue_btn.click()
+    sleep(3)
 
-    # # Verify Cart Items
-    # verify_cart_card = driver.find_element(By.XPATH, '//body/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]')
-    # assert verify_cart_card.is_displayed()
+    ### VERIFY USER IS NAVIGATED TO CONFIRM ORDER PAGE ###
 
-    # # Verify Product List
-    # verify_product_list_1 = driver.find_element(By.XPATH, '//body[1]/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/a[1]')
-    # assert verify_product_list_1.text == 'HTC Touch HD'
-    # verify_product_list_2 = driver.find_element(By.XPATH, '//body[1]/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/a[1]')
-    # assert verify_product_list_2.text == 'iMac'
+    assert driver.current_url == "https://ecommerce-playground.lambdatest.io/index.php?route=extension/maza/checkout/confirm"
+    get_title = "Confirm Order"
+    assert get_title in driver.title
+    assert "Confirm Order" in driver.page_source
+    assert "Payment Address" in driver.page_source
+    assert "Shipping Address" in driver.page_source
+    assert "Shipping Method:" in driver.page_source
 
-    # check_btn = driver.find_element(By.XPATH, "//body/div[1]/div[5]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[5]/label[1]")
-    # check_btn.click()
+    product_information = driver.find_element(By.CLASS_NAME, 'table-bordered')
+    assert product_information.is_displayed()
+    assert "HTC Touch HD" in product_information.text
+    assert "iMac" in product_information.text
 
-    # continue_btn = driver.find_element(By.XPATH, "//button[@id='button-save']")
-    # continue_btn.click()
-    # sleep(3)
+    tbody = driver.find_element(By.XPATH, '//table/tbody')
+    rows_at_index_2 = tbody.find_elements(By.XPATH, './tr')
+    expected_length = 2
+    assert len(rows_at_index_2) == expected_length, f"Expected length: {expected_length}, Actual length: {len(rows_at_index_2)}"
 
-    # ### VERIFY USER IS NAVIGATED TO CONFIRM ORDER PAGE ###
+    # Payment Address
+    card_bodies = driver.find_elements(By.CLASS_NAME, 'card-body')
+    expected_text = 'Naufal Azhar\nPt.mamkmur\njalan1\njalan2\njakarta 1234\nJawa Barat,Indonesia'
+    actual_text = card_bodies[0].text
+    assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
 
-    # assert driver.current_url == "https://ecommerce-playground.lambdatest.io/index.php?route=extension/maza/checkout/confirm"
-    # get_title = "Confirm Order"
-    # assert get_title in driver.title
-    # assert "Confirm Order" in driver.page_source
-    # assert "Payment Address" in driver.page_source
-    # assert "Shipping Address" in driver.page_source
-    # assert "Shipping Method:" in driver.page_source
+    # Shipping Address
+    expected_text = 'Naufal Azhar\nPt.mamkmur\njalan1\njalan2\njakarta 1234\nJawa Barat,Indonesia'
+    actual_text = card_bodies[1].text
+    assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
 
-    # product_information = driver.find_element(By.CLASS_NAME, 'table-bordered')
-    # assert product_information.is_displayed()
-    # assert "HTC Touch HD" in product_information.text
-    # assert "iMac" in product_information.text
+    # Shipping Method
+    expected_text = 'Flat Shipping Rate'
+    actual_text = card_bodies[2].text
+    assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
 
-    # tbody = driver.find_element(By.XPATH, '//table/tbody')
-    # rows_at_index_2 = tbody.find_elements(By.XPATH, './tr')
-    # expected_length = 2
-    # assert len(rows_at_index_2) == expected_length, f"Expected length: {expected_length}, Actual length: {len(rows_at_index_2)}"
+    confirm_btn = driver.find_element(By.XPATH, "//button[@id='button-confirm']")
+    confirm_btn.click()
+    sleep(3)
 
-    # # Payment Address
-    # card_bodies = driver.find_elements(By.CLASS_NAME, 'card-body')
-    # expected_text = 'Naufal Azhar\nPt.mamkmur\njalan1\njalan2\njakarta 1234\nJawa Barat,Indonesia'
-    # actual_text = card_bodies[0].text
-    # assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
+    assert driver.current_url == "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/success"
+    assert "Your order has been placed!" in driver.title
 
-    # # Shipping Address
-    # expected_text = 'Naufal Azhar\nPt.mamkmur\njalan1\njalan2\njakarta 1234\nJawa Barat,Indonesia'
-    # actual_text = card_bodies[1].text
-    # assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
-
-    # # Shipping Method
-    # expected_text = 'Flat Shipping Rate'
-    # actual_text = card_bodies[2].text
-    # assert expected_text == actual_text, f"Expected: {expected_text}, Actual: {actual_text}"
-
-    # confirm_btn = driver.find_element(By.XPATH, "//button[@id='button-confirm']")
-    # confirm_btn.click()
-    # sleep(3)
-
-    # assert driver.current_url == "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/success"
-    # assert "Your order has been placed!" in driver.title
-
-    # success_msg = "Your order has been placed!"
-    # assert success_msg in driver.find_element(By.TAG_NAME, "h1").text
-    # assert "Your order has been successfully processed!" in driver.page_source
-    # sleep(5)
+    success_msg = "Your order has been placed!"
+    assert success_msg in driver.find_element(By.TAG_NAME, "h1").text
+    assert "Your order has been successfully processed!" in driver.page_source
+    sleep(5)
